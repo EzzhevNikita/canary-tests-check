f9ee9aef03e0009aca37ba563ca10ac19cb21ae8
Nikita Ezzhev <v-niezz@microsoft.com>
2021-01-19 15:50:20 +0300

    Update send-signal.sh

--- src/cmdLine/scripts/send-signal.sh
+++ src/cmdLine/scripts/send-signal.sh
@@ -5,12 +5,12 @@ signal_caught_sigterm() {
   exit 0
 }
 
-# trap signal_caught_sigterm SIGTERM
+trap signal_caught_sigterm SIGTERM
 counter=0;
 
 while true;
 do
-  while :; do echo 'Hit CTRL+C'; sleep 1; done
+  # while :; do echo 'Hit CTRL+C'; sleep 1; done
   if [[ $counter -eq 10 ]];
   then
     ps -ef | grep 'CmdLine_' | grep -v grep | awk '{print $2}' | xargs -r kill -2
